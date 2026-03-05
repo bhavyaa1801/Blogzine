88ceab77c17e5ba42a60ba547c10fcfa07e3a646
Bhavya <rajputbhavya71@gmail.com>
2026-03-05 21:42:54 +0530

    changed f lname

--- src/pages/AddPostPage.jsx
+++ src/pages/AddPostPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, PostForm } from '../components'
 
-function AddPostPage() {
+function AddPost() {
   return (
     <div className='py-8'>
         <Container>
@@ -11,4 +11,4 @@ function AddPostPage() {
   )
 }
 
-export default AddPostPage
+export default AddPost
